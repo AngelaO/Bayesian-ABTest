2408fbd7fb1ef828238bdcb5ca73dc3bd4e5f5d2
Mariana Souza <13593063+PukkaPad@users.noreply.github.com>
2019-03-20 20:44:07 +0000

    First commit
    setup.py file - needs changing

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+with open(“requirements.txt”) as req_txt:
+	required = [line for line in req_txt.read().splitlines() if line]
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name=" pysprint_abBay_analysis",
+    version="0.0.1",
+    author="Gender Minorities Py Sprints",
+    author_email="author@example.com",
+    description="Bayesian AB test package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
