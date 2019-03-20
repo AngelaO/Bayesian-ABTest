cc4806d34ff1e4d55c935c5f22d9985f32a90bd6
Arundhati Santhini <arundathisanthini@gmail.com>
2019-03-20 21:24:53 +0000

    calculate_ab function v1

--- calculate_ab.py
+++ calculate_ab.py
@@ -1,2 +1,17 @@
 # Calculate the bayesian_AB function and return the option with gretaer probability
 
+from scipy.special import beta
+
+def calculate_ab(alpha_A,beta_A,alpha_B,beta_B):
+  prefer_b_probability=0.0
+  for i in range(alpha_B):
+    num=beta(alpha_A+i,(beta_B+beta_A))
+    den=(beta_B+i)*beta(1+i,beta_B)*beta(alpha_A,beta_A)
+    result=num/den
+    prefer_b_probability+=result
+  return prefer_b_probability
+
+if(calculate_ab(aA,bA,aB,bB)>=0.5):
+  return b
+else:
+  return a
