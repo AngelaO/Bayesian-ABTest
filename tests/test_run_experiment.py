8806b6a12ba6525f0bd560453f6e1b32cf5fa37d
Angela Onslow <angela@erevalue.com>
2019-03-20 21:09:03 +0000

    Fixed typo in isinstance

--- tests/test_run_experiment.py
+++ tests/test_run_experiment.py
@@ -1,11 +1,12 @@
 import bayesian_ab_testing
 
+
 def test_run_experiment_a():
     res = bayesian_ab_testing.run_experiment_a()
-    assert isintance(res, int)
+    assert isinstance(res, int)
     assert res == 0 or res == 1
 
 def test_run_experiment_b():
     res = bayesian_ab_testing.run_experiment_b()
-    assert isintance(res, int)
+    assert isinstance(res, int)
     assert res == 0 or res == 1
